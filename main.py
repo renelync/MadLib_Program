95144fbf725b2795e2fb77b770499c87b09eff1c
renelync <99835887+renelync@users.noreply.github.com>
2022-02-27 15:28:06 -0800

    added in methods for upper & capitalize
    and some print statements to bring them out into play

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ print(dictionary[0]["Adjectives"])
 print(dictionary[0]["Verbs"])
 print(dictionary[0]["Adverbs"])
 print("\nAll righty then! Now that we understand those words, let's make a MadLib together! You'll need to provide me with some information below.\n")
+
+
+city_name = input(f"First, what city would you like your main character to be from? "
+
 #list to inquire for words from user
 story_parts = [
   "1st noun",
@@ -32,10 +36,11 @@ bucket_for_story_parts = {}
 #loop that fills story parts list
 for part in story_parts:
   #asks the user for different word types
-  word_choice = input(f"Please enter {part}: ")
+  word_choice = input(f"Next, please enter {part}: ")
   #places each chosen word inside the dictionary. It turns each word type into a key, while the user's word choice is that key's value.
   bucket_for_story_parts[part] = word_choice
 
 # print(bucket_for_story_parts)
-print("\nThank you for providing that information! Enjoy your MadLib!\n")
-print(f"Once upon a time, there was a young little {bucket_for_story_parts['1st noun']} who grew up in a town where he was no one’s favorite. Everyday, he woke up craving for love and attention from someone, feeling so lonely… He knew he didn’t know how to get anyone to love him, so he settled for getting attention by {bucket_for_story_parts['1st adverb, ending in -ly']} committing {bucket_for_story_parts['1st adjective']} acts he knew would only get him into trouble. All he wanted was some other young {bucket_for_story_parts['1st noun']} to share {bucket_for_story_parts['number']} of his favorite bowl(s) of {bucket_for_story_parts['your favorite food']} with from the oldest {bucket_for_story_parts['1st noun']}, who was also the best chef, in town. How he {bucket_for_story_parts['1st verb, ending in -ed']} the smell coming from that bowl, and he {bucket_for_story_parts['2nd adverb, ending in -ly']} wanted to share it with someone. One day, he found some {bucket_for_story_parts['2nd noun']} lying on the ground, and it was just enough to trade it for {bucket_for_story_parts['number']} bowl(s) of his favorite food in the whole wide world. He knew he didn’t want to eat it all alone anymore, though… So, he {bucket_for_story_parts['2nd verb, ending in -ed']} up the courage to ask another young {bucket_for_story_parts['1st noun']} he passes by everyday. To his surprise, they said they would love to join him. Because, of course, free food! From that day on, he was no longer a lonely little {bucket_for_story_parts['1st noun']}. He finally found another young {bucket_for_story_parts['1st noun']} to share his favorite food with.")
+story_time = "awesome job! here's your madlib!"
+print(f"{story_time.upper}"
+print(f"Once upon a time, there was a young little {bucket_for_story_parts['1st noun']} who grew up in the town of {city_name.capitalize} where he was no one’s favorite. Everyday, he woke up craving for love and attention from someone, feeling so lonely… He knew he didn’t know how to get anyone to love him, so he settled for getting attention by {bucket_for_story_parts['1st adverb, ending in -ly']} committing {bucket_for_story_parts['1st adjective']} acts he knew would only get him into trouble. All he wanted was some other young {bucket_for_story_parts['1st noun']} to share {bucket_for_story_parts['number']} of his favorite bowl(s) of {bucket_for_story_parts['your favorite food']} with from the oldest {bucket_for_story_parts['1st noun']}, who was also the best chef, in town. How he {bucket_for_story_parts['1st verb, ending in -ed']} the smell coming from that bowl, and he {bucket_for_story_parts['2nd adverb, ending in -ly']} wanted to share it with someone. One day, he found some {bucket_for_story_parts['2nd noun']} lying on the ground, and it was just enough to trade it for {bucket_for_story_parts['number']} bowl(s) of his favorite food in the whole wide world. He knew he didn’t want to eat it all alone anymore, though… So, he {bucket_for_story_parts['2nd verb, ending in -ed']} up the courage to ask another young {bucket_for_story_parts['1st noun']} he passes by everyday. To his surprise, they said they would love to join him. Because, of course, free food! From that day on, he was no longer a lonely little {bucket_for_story_parts['1st noun']}. He finally found another young {bucket_for_story_parts['1st noun']} to share his favorite food with.")
